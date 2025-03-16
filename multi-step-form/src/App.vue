fbe45ff50a9cf5823bc4862952c3bf6bd140ef10
akareagwom <akareazikang20@gmail.com>
2025-03-16 15:29:28 +0100

    mobile step1 form

--- multi-step-form/src/App.vue
+++ multi-step-form/src/App.vue
@@ -4,9 +4,12 @@ import StepOne from './components/StepOne.vue';
 </script>
 
 <template class="bg-[hsl(213, 96%, 18%)] w-[100%] bg-[red] h-[100vh]">
+  <img src="" alt="" srcset="">
   <div class="hsl(228, 100%, 84%) bg-teal-50 h-[100vh]">
-    <HelloWorld msg="" class="w-full main" />
-    <StepOne msg="" class="w-full main" />
+    <!-- <HelloWorld msg="" class="w-full main" /> -->
+     <div class="p-4">
+       <StepOne msg="" class="w-full main" />
+     </div>
     <Footer></Footer>
   </div>
 </template>
--- multi-step-form/src/components/StepOne.vue
+++ multi-step-form/src/components/StepOne.vue
@@ -1,14 +1,14 @@
     <template>
-    <div class='bg-white w-[85%] absolute top-[15%] left-[5%] shadow p-4 rounded-[10px] flex flex-col items-center'>
+    <div class='bg-white justify-center shadow p-4 rounded-[10px] flex flex-col items-center'>
         <h1 class="text-3xl text-left text-black">Personal info</h1>
         <p class="text-left p-4">Please provide your name, email address, and phone number</p>
         <form class="flex flex-col">
             <label>Name</label>
-            <input type="text" v-model="name" placeholder="e.g.Stephen King" class="px-12 py-4 border-2 border-[hsl(213, 96%, 18%)] rounded-[5px] border-solid"/>
+            <input type="text" placeholder="e.g.Stephen King" class="px-12 py-4 border-2 border-[hsl(213, 96%, 18%)] rounded-[5px] border-solid"/>
             <label>Email Address</label>
-            <input type="text" v-model="name" placeholder="e.g.stephenking@lorem.com" class="px-12 py-4 border-2 border-[hsl(213, 96%, 18%)] rounded-[5px] border-solid"/>
+            <input type="text" placeholder="e.g.stephenking@lorem.com" class="px-12 py-4 border-2 border-[hsl(213, 96%, 18%)] rounded-[5px] border-solid"/>
             <label>Phone Number</label>
-            <input type="text" v-model="name" placeholder="e.g.+1 234 567 890" class="px-12 py-4 border-2 border-[hsl(213, 96%, 18%)] rounded-[5px] border-solid"/>
+            <input type="text" placeholder="e.g.+1 234 567 890" class="px-12 py-4 border-2 border-[hsl(213, 96%, 18%)] rounded-[5px] border-solid"/>
         </form>
     </div>
     </template>
